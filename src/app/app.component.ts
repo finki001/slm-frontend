f5f561285bad32026a4d74513e1778c5e632ce3e
Lukas Fink <lukas.fink@troii.com>
2022-10-05 14:24:25 +0200

    slm-frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {MaintenanceService} from "./maintenance.service";
+import {Subscription, switchMap, timer} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'slm-frontend';
+
+  subscription: Subscription;
+
+  backgroundColor = 'green';
+
+  constructor(private maintenanceService: MaintenanceService) {
+    this.subscription = timer(0, 5000).pipe(
+      switchMap(() => this.maintenanceService.getMessage())
+    ).subscribe(message => this.backgroundColor = !message ? 'green' : 'red');
+
+    maintenanceService.getMessage().subscribe(console.log);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
